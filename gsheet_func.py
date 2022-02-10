495c85f9493b585b2c3115d27476c955646890c9
EmiHurtado <86236241+EmiHurtado@users.noreply.github.com>
2022-02-10 14:52:11 -0600

    Update gsheet_func.py

--- gsheet_func.py
+++ gsheet_func.py
@@ -2,34 +2,25 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
-"""
-Se autentifica y permite el acceso a las hojas de datos.
-"""
+# Se autentifica y permite el acceso a las hojas de datos.
 s=['https://www.googleapis.com/auth/spreadsheets',
 'https://www.googleapis.com/auth/drive']
 
-creds= ServiceAccountCredentials.from_json_keyfile_name("credentials.json",s)
+creds= ServiceAccountCredentials.from_json_keyfile_name('C:/Users/cafe_/OneDrive/Escritorio/Trabajo/Kairos/Tecnologia/Chatbot/Version_3/whatsapp-bot/credentials.json',s)
 client=gspread.authorize(creds)
 
 # Se accede a la hoja de datos para guardar información.
 sheet = client.open("Reminders").sheet1
-row_values=sheet.row_values(1)
-col_values=sheet.col_values(1)
-row_filled=len(col_values)
-col_filled=len(row_values)
 
-"""
-Se definen las funciones para guardar datos.
-"""
-def save_reminder_date(date):
-
-    sheet.update_cell(row_filled+1, 1, date)
-    print("Fecha guardada!")
-    return 0
+# Se definen las funciones para recuperar datos.
+def recuperador(nombre):
     
-def save_reminder_body(msg):
+    # Encuentra la celda requerida
+    cell = sheet.find(nombre)
 
-    sheet.update_cell(row_filled+1, 2, msg)
-    print("Mensaje guardado!")
-    return 0
+    # Imprime la información deseada
+    val = sheet.cell(cell.row, cell.col + 1).value
+    print("Usted está", val)
+    return val
+
 
