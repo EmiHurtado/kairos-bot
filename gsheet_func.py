0609f57d33c021c85f8a55dee64c0bc14884d460
EmiHurtado <86236241+EmiHurtado@users.noreply.github.com>
2022-02-22 14:25:01 -0600

    Update gsheet_func.py

--- gsheet_func.py
+++ gsheet_func.py
@@ -1,6 +1,7 @@
 # Librerías
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
+import random
 
 # Se autentifica y permite el acceso a las hojas de datos.
 s=['https://www.googleapis.com/auth/spreadsheets',
@@ -11,6 +12,7 @@ client=gspread.authorize(creds)
 
 # Se accede a la hoja de datos para guardar información.
 sheet = client.open("Data_Base").sheet1
+sheet2 = client.open("Data_Base").sheet2
 
 # Se definen las funciones para recuperar datos.
 def recuperador(nombre):
@@ -32,5 +34,15 @@ def recuperador(nombre):
 
     else:
         return "No se pudo encontrar. Contáctese con RH."
+    
+# Se definen las funciones para recuperar datos.
+def reclutador():
+    
+    # Genera un número aleatorio.
+    n = random.randint(2,17)
 
-
+    # Encuentra al reclutador y devuelve su información.
+    nombre = sheet2.cell(n, 3).value
+    numero = sheet2.cell(n, 10).value
+    
+    return nombre + " se contactará con usted. Este es su número telefónico: " + numero
