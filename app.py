02984ee5648b83e411e51fd340db48f3f80b49f1
EmiHurtado <86236241+EmiHurtado@users.noreply.github.com>
2022-02-22 13:35:10 -0600

    Update app.py

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def reply(): # Se define el comportamiento de la respuesta.
         message.body(reply)
         responded = True
 
-    elif split[0] == "T" or split[0] == "C":
+    elif incoming_msg[0] == "T" or incoming_msg[0] == "C":
         reply = recupera_local(incoming_msg2)
         message.body(reply)
         responded = True 
