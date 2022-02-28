dd214411ff6a90593a036e4ae8b533c2ee5040cd
EmiHurtado <86236241+EmiHurtado@users.noreply.github.com>
2022-02-28 15:35:59 -0600

    Update gsheet_func.py

--- gsheet_func.py
+++ gsheet_func.py
@@ -13,6 +13,13 @@ client=gspread.authorize(creds)
 # Se accede a la hoja de datos para guardar información.
 sheet = client.open("Data_Base").sheet1
 sheet2 = client.open("Data_Base").worksheet("Hoja 2")
+sheet3 = client.open("Data_Base").worksheet("Hoja 3")
+
+# Define las casillas ya ocupadas
+row_values = sheet3.row_values(1)
+col_values = sheet3.col_values(1)
+row_filled = len(col_values)
+col_filled = len(row_values)
 
 # Se definen las funciones para recuperar datos.
 def recuperador(nombre):
@@ -46,3 +53,28 @@ def reclutador():
     numero = sheet2.cell(n, 10).value
     
     return nombre + " se contactará con usted. Este es su número telefónico: " + numero
+
+# Funciones para guardar los datos
+def upNombre(nombre):
+
+    sheet3.update_cell(row_filled+1, 1, nombre)
+    print("Nombre listo")
+    return 0
+    
+def upCV(CV):
+
+    sheet3.update_cell(row_filled+1, 2, CV)
+    print("CV listo")
+    return 0
+
+def upFecha(fecha):
+
+    sheet3.update_cell(row_filled+1, 3, fecha)
+    print("Fecha lista")
+    return 0
+
+def upHora(hora):
+
+    sheet3.update_cell(row_filled+1, 4, hora)
+    print("Hora lista")
+    return 0
